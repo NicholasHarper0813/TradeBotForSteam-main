8653befe2039294e9149418e82b5f6eeedaa0ecb
nicholas harper <nicholasharper000@gmail.com>
2024-12-16 08:57:48 -0800

    Update configSecret.js

--- WebPage/configs/scripts/configSecret.js
+++ WebPage/configs/scripts/configSecret.js
@@ -1,25 +1,30 @@
-const configjs = require('../../../modules/config');
 const config = require('../../../config.json');
+const configjs = require('../../../modules/config');
 const electron = require('electron');
 const shell = electron.shell;
-const iSecret = document.getElementById('iSecret');
-const sSecret = document.getElementById('sSecret');
-iSecret.value = config.identitySecret;
-sSecret.value = config.sharedSecret;
 const label = document.getElementById('label');
-let btn = document.getElementById('btn');
+const sSecret = document.getElementById('sSecret');
+const iSecret = document.getElementById('iSecret');
+sSecret.value = config.sharedSecret;
+iSecret.value = config.identitySecret;
+
 let externalLink = document.getElementById('link');
+let btn = document.getElementById('btn');
+
 link.addEventListener('click', () => {
     shell.openExternal('https://github.com/SteamTimeIdler/stidler/wiki/Getting-your-%27shared_secret%27-code-for-use-with-Auto-Restarter-on-Mobile-Authentication#getting-shared-secret-from-steam-desktop-authenticator-windows');
 });
+
 btn.addEventListener('click', () => {
-    if (iSecret.value !== '') {
+    if (iSecret.value !== '') 
+    {
         configjs.confighandler('identitySecret', iSecret.value);
     }
-    if(sSecret.value !== ''){
+    if(sSecret.value !== '')
+    {
         configjs.confighandler('sharedSecret', sSecret.value);
     }
+    
     window.close();
     label.innerHTML = 'Updated values';
-
 });
