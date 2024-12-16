33758e6b89652e7b44ab40233d35a6feb90e7659
nicholas harper <nicholasharper000@gmail.com>
2024-12-16 08:46:11 -0800

    Update config.js

--- modules/config.js
+++ modules/config.js
@@ -1,14 +1,14 @@
-//Gets and sends the configs.
-let fs = require('fs'); //Requires fs for filesystem and editing config.json file
-let array = require('../config.json'); //Import config.json file into javascript file.
+let fs = require('fs');
+let array = require('../config.json');
 
-module.exports.confighandler = function confighandler (label, data) { //Creates function that takes in a label and some data that it will change to inside config.json
-    console.log('Changing the config file...');
+module.exports.confighandler = function confighandler (label, data) 
+{
+    //console.log('Changing the config file...');
     console.log(label + ' changed to: ' + data);
-    array[label] = data; //Changes the selected data.
-    let json = JSON.stringify(array);//Stringifies it.
+    array[label] = data;
+    let json = JSON.stringify(array);
     fs.writeFile('config.json', json, 'utf8', (err) => {
         if(err) throw err;
         return;
-    });//Writes it back to the config.json file.   
+    }); 
 }
