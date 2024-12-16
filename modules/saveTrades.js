3e1bdbeac66e075af8d89f8cbbd60d7a2e0dd973
nicholas harper <nicholasharper000@gmail.com>
2024-12-16 08:45:30 -0800

    Update saveTrades.js

--- modules/saveTrades.js
+++ modules/saveTrades.js
@@ -1,13 +1,14 @@
 const debug = require('./debug');
 const fs = require('fs');
 module.exports = async (partner, offerid, profit, lostItems, gainedItems) => {
-    //CONSTRUCT THE JSON FILE
-    let json = {
+    let json = 
+    {
         partner: partner,
         profit: profit,
         lostItems: lostItems,
         gainedItems: gainedItems
     }
+    
     json = JSON.stringify(json);
     fs.writeFile(`./trades/${offerid}.json`, json, err => debug(err));
 }
