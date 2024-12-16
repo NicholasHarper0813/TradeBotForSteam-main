74cfb221d8474585bf5ac3d2ec4be32e9d48da4c
nicholas harper <nicholasharper000@gmail.com>
2024-12-16 08:52:39 -0800

    Update profitRate.js

--- modules/dataHandling/profitRate.js
+++ modules/dataHandling/profitRate.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 const debug = require('../debug');
 module.exports = () => {
-    //SET PROFIT RATE into the data file.
     fs.readdir('../../trades/', (err, files) => {
         if(err) debug(err);
         let accepted;
