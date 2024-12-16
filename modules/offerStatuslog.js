8d546e3969b73a4c74851062f8ce1958a9e360d5
nicholas harper <nicholasharper000@gmail.com>
2024-12-16 08:44:57 -0800

    Update offerStatuslog.js

--- modules/offerStatuslog.js
+++ modules/offerStatuslog.js
@@ -1,13 +1,11 @@
 module.exports = (status, profit) => {
-// FALSE = NOT ACCEPTED
-// TRUE = ACCEPTED
-// This file is for writing the logs if an offer is accepted or not.
-    if(status === true){
-        console.log('Accepted with profit: '.green + profit); //Logs the profit made by the trade
-        console.log('================================================'.green);
-    } else {
-        console.log('Declined the offer.'.red); //Logs that it's declines the offer
-        console.log('================================================'.green);
+    if(status === true)
+    {
+        console.log('Accepted with profit: '.green + profit);
+    } 
+    else 
+    {
+        console.log('Declined the offer.'.red);
     }
 
 }
