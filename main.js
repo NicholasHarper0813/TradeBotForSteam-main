aad852a40253226fe3f8592bed114db5a15b2d7b
nicholas harper <nicholasharper000@gmail.com>
2024-12-16 09:04:14 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,42 +1,38 @@
 const config = require('./config.json');
+const configjs = require('./modules/config.js');
 const SteamTotp = require('steam-totp');
 const SteamUser = require('steam-user');
 const SteamCommunity = require('steamcommunity');
 const TradeOfferManager = require('steam-tradeoffer-manager');
 const market = require('./modules/steam-market-pricing');
 const debug = require('./modules/debug.js');
+const electron = require('electron');
+const remote = require('electron').remote;
+const path = require('path');
+const url = require('url');
 const fs = require('fs');
 const colors = require('colors');
 const underscore = require('underscore');
 const readline = require('readline');
-const electron = require('electron');
-const configjs = require('./modules/config.js');
-const remote = require('electron').remote;
-const path = require('path');
-const url = require('url');
 const readValue = require('./modules/readvalue.js');
 const offerStatusLog = require('./modules/offerStatuslog.js');
 const mainMenuTemplateModule = require('./modules/templates/template');
 const processOffer = require('./modules/offerHandling/processOffer')
 const { app, BrowserWindow, Menu, ipcMain } = electron;
 
-let win; //Sets up temporary variable that will be set later in the script.
-let ownerID = config.ownerID;
+let win;
+let trash = config.trashlimit;
 let gameid = config.game;
-let trash = config.trashlimit; //Sets up the trash limit to a custom variable.d
+let ownerID = config.ownerID;
 let partner, theirprice, ourprice, ourItems, theirItems,ourValue,theirValue, allitems, allourItems;
 
-debug("Loaded debug.");
-const client = new SteamUser(); //CREATES A NEW CLIENT FOR SteamTotp
+const client = new SteamUser();
 const community = new SteamCommunity();
-
 const manager = new TradeOfferManager({
   steam: client,
   community: community,
   language: 'en'
 });
-
-debug("New tradeoffer-manager was setup.");
 
 console.log('This bot was developed by CloudiaN'.cyan);
 console.log('Verision 1.2.4'.cyan);
@@ -51,35 +47,29 @@ const logOnOptions = {
 };
 
 client.logOn(logOnOptions);
-debug("Tried to logon with account credentials");
 client.on('loggedOn', () => {
   console.log(`Logged into steam with account: ${config.username}`.green); //Displays the name of the account that's logged in.
   console.log('\n');
   console.log('Skin trash limit set to: ' + config.trashlimit);
   client.setPersona(1); //Shows that the bot is online.
   client.gamesPlayed(config.GameTitle);
-  debug("Logged on to steam");
 });
 
 app.on('ready', function () 
 {
-  debug("Loaded App");
   win = new BrowserWindow({ width: 800, height: 600, icon: __dirname + '/img/bot.png', show: true }); //Initialized a new window.
   win.loadURL(url.format({
     pathname: path.join(__dirname + '/WebPage/index.html'),
     protocol: 'file',
     slashes: true
   }));
-  debug("Loaded BrowserMenu");
   const mainMenu = Menu.buildFromTemplate(mainMenuTemplateModule);
   Menu.setApplicationMenu(mainMenu);
-
 });
 
 app.on('window-all-closed', function () 
 {
   if (process.platform !== 'darwin') {
-    debug("App Shutdown");
     app.quit();
   }
 });
@@ -106,6 +96,7 @@ manager.on('newOffer', (offer) => {
     debug("Offer was done processing.")
   });
 });
+
 ipcMain.on('configGames', (event, data) => {
   let newgame = data.game;
   console.log(data);
